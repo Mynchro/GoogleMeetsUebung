f19456f82672ef900457e52db0c81320800cfae2
Maik <m.g.artist@hotmail.de>
2024-07-10 15:07:07 +0200

    icon gewusel behoben, fast

--- NebulaMeets/src/App.jsx
+++ NebulaMeets/src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+import ControlBar from "./components/ControlBar"
 import "./App.css";
+import './components/ControlBar.css'
 
 function App() {
   return (
     <>
       <h1>Hello Nebula Odyssey</h1>
+      <ControlBar/>
     </>
   );
 }
