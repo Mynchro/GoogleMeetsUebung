98b74f9dfb17daaf68cee25a55a978ba81ddfdfb
Marius <webermarius1995@gmail.com>
2024-07-11 15:12:05 +0200

    komponenten geschlossen

--- NebulaMeets/src/App.jsx
+++ NebulaMeets/src/App.jsx
@@ -2,16 +2,16 @@ import "./App.css";
 import Bilder from "./components/Bilder";
 import Header from "./components/Header";
 import Emoji from "./components/Emoji";
-import ControlBar from "./components/ControlBar"
-import './components/ControlBar.css'
+import ControlBar from "./components/ControlBar";
+import "./components/ControlBar.css";
 
 function App() {
   return (
     <>
       <Header />
-      <Bilder></Bilder>
-      <Emoji></Emoji>
-      <ControlBar/>
+      <Bilder />
+      <Emoji />
+      <ControlBar />
     </>
   );
 }
